a47a8a29b223b060e70cefd68efeaee091f0d381
IlyaKrn <ilyakornoukhov@gmail.com>
2025-06-05 20:07:18 +0300

    href fix

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,12 +6,12 @@
             </a>
         </div>
         <div class="nav-links">
-            <a href="index.blade.php" class="nav-button">Home</a>
-            <a href="software.blade.php" class="nav-button active">Software</a>
-            <a href="devs.blade.php" class="nav-button">Developers</a>
-            <a href="about-us.blade.php" class="nav-button">About Us</a>
-            <a href="register.blade.php" class="nav-button register-btn">Register</a>
-            <a href="login.blade.php" class="nav-button login-btn">Login</a>
+            <a href="/" class="nav-button">Home</a>
+            <a href="/software" class="nav-button active">Software</a>
+            <a href="/devs" class="nav-button">Developers</a>
+            <a href="/about-us" class="nav-button">About Us</a>
+            <a href="/register" class="nav-button register-btn">Register</a>
+            <a href="/login" class="nav-button login-btn">Login</a>
         </div>
     </nav>
 </header>
